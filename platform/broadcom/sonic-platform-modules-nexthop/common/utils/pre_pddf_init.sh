3ee65896a837a4b6b2daa0c175ff863ef174bdca
Lotus Fenn <lotus@nexthop.ai>
2025-10-16 15:27:04 +0000

    disable amd i2c v2

--- platform/broadcom/sonic-platform-modules-nexthop/common/utils/pre_pddf_init.sh
+++ platform/broadcom/sonic-platform-modules-nexthop/common/utils/pre_pddf_init.sh
@@ -13,16 +13,18 @@ log() {
 nh_gen pddf_device_json
 nh_gen pcie_yaml
 
-if lsmod | grep -q "i2c_designware_platform"; then
+if [ -e /sys/bus/i2c/devices/i2c-0 ]; then
   modprobe -r i2c_designware_platdrv 2>/dev/null || true
   modprobe -r i2c_designware_platform 2>/dev/null || true
   modprobe -r i2c_designware_core 2>/dev/null || true
   modprobe -r i2c_piix4 2>/dev/null || true
+  modprobe -r i2c_asf 2>/dev/null || true
   cat << EOF > /etc/modprobe.d/blacklist-amd-i2c.conf
 blacklist i2c_designware_platdrv
 blacklist i2c_designware_platform
 blacklist i2c_designware_core
 blacklist i2c_piix4
+blacklist i2c_asf
 EOF
   update-initramfs -u
 fi
